c61411149389037e05f375fbe6e86cc5ac3dc7c1
Prathamesh Kulkarni <kulkarni@fim.uni-passau.de>
2024-04-11 16:37:05 +0200

    FEAT: Created base page for admin dashboard

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -41,7 +41,7 @@ export const StoreModal = () => {
             setLoading(true);
             const response = await axios.post("/api/stores", values);
 
-            toast.success("Store created successfully");
+            window.location.assign(`/${response.data.id}`);
         } catch (error) {
             toast.error("Failed to create store");
         } finally {
